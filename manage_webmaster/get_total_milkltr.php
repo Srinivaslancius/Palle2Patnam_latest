a12d6ebcc9b4edfbf264736bba7047aac67c4b61
Gunavardhan <vardhan.gujjula@gmail.com>
2017-09-12 18:37:54 +0530

    get total milkltr page added

--- manage_webmaster/get_total_milkltr.php
+++ manage_webmaster/get_total_milkltr.php
@@ -1,5 +1,5 @@
 <?php
-include_once('includes/config.php');
+include_once('admin_includes/config.php');
 $user_id =  $_POST['user_id'];
 $sql = "SELECT total_ltr FROM milk_orders WHERE user_id = '$user_id' AND MONTH(`start_date`) = MONTH(CURDATE()) AND YEAR(`start_date`) = YEAR(CURDATE()) ";
 $result = $conn->query($sql);
