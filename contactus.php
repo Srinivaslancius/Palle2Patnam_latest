a9e462d201fddf4dc7ccaa7dc97c2c1ba6f28556
srinivaslancius <srinivas@lanciussolutions.com>
2017-09-15 12:53:59 +0530

    changed contact us page and created slider page

--- contactus.php
+++ contactus.php
@@ -53,50 +53,7 @@ $msgClass = '';
 ?>
 
         <!--header area end-->
-
-        <!--welcome area Start-->
-        <div class="welcome_area">
-            <div class="welcome_slider">
-                <div class="single_welcome_slider" style="background-image:url(img/welcome_bg.jpg)">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-7 col-sm-8">
-                                <div class="welcome_content">
-                                    <h1>Neatly. Delightedly. Quietly.</h1><br>
-									<span>Get Your Fresh Grocery Delivered to You whenever you want it.</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="single_welcome_slider" style="background-image:url(img/welcome_bg2.jpg)">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-7 col-sm-8">
-                                <div class="welcome_content">
-									<h1>Neatly. Delightedly. Quietly.</h1>
-									<span>Get Your Fresh Grocery Delivered to You whenever you want it.</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="single_welcome_slider" style="background-image:url(img/welcome_bg3.jpg)">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-7 col-sm-8">
-                                <div class="welcome_content">
-                                    <h1>Neatly. Delightedly. Quietly.</h1>
-									<span>Get Your Fresh Grocery Delivered to You whenever you want it.</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--welcome area end-->
-    </div>
+    <?php include_once 'slider.php';?>
     <!--booking area start-->
     <div class="booking_area parallax"style="margin-top:2px;">
         <div class="container">
