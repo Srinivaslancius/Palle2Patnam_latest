938495be36616feaf3bbd5703d89f940021fc463
Srinivaslancius <srinivas@lanciussolutions.com>
2017-09-20 15:00:20 +0530

    p2p new pdf chnages

--- manage_webmaster/add_cancel_milk_order.php
+++ manage_webmaster/add_cancel_milk_order.php
@@ -3,22 +3,30 @@
 if (!isset($_POST['submit']))  {
     echo " ";
 } else {
-        //echo "<pre>"; print_r($_POST); die;
+      //echo "<pre>"; print_r($_POST); die;
       $user_id = $_POST['user_id'];
       $product_id = $_POST['product_id'];
       $total_ltr = $_POST['total_ltr'];
       $cancel_ltr = $_POST['cancel_ltr'];
-      $date = date_create($_POST['cancel_date']);
-      $cancel_date = date_format($date,"Y-m-d");            
-      $created_at = date("Y-m-d h:i:s");          
-          //$status = $_POST['status'];                                           
-      $sql = "INSERT INTO cancel_milk_orders (`user_id`, `product_id`, `total_ltr`, `cancel_ltr`, `cancel_date`, `created_at`) VALUES ('$user_id','$product_id', '$total_ltr','$cancel_ltr', '$cancel_date', '$created_at')";
-      if($conn->query($sql) === TRUE){
-        echo "<script type='text/javascript'>window.location='cancel_milk_orders.php?msg=success'</script>";
+      $getDates = explode(",",$_POST['cancel_date']);
+      $created_at = date("Y-m-d h:i:s");      
+      
+      foreach($getDates as $key=>$value){
 
+        $cancel_date = $getDates[$key];
+        //echo "<pre>"; print_r($getDates); die;
+        $sql = "INSERT INTO cancel_milk_orders (`user_id`, `product_id`, `total_ltr`, `cancel_ltr`, `cancel_date`, `created_at`) VALUES ('$user_id','$product_id', '$total_ltr','$cancel_ltr', '$cancel_date', '$created_at')";
+        
+        $result = $conn->query($sql);
+
+      }     
+
+      if( $result == 1){
+        echo "<script type='text/javascript'>window.location='cancel_milk_orders.php?msg=success'</script>";
       } else {
-        echo "<script type='text/javascript'>window.location='cancel_milk_orders.php?msg=fail'</script>";
-      }          
+          echo "<script type='text/javascript'>window.location='cancel_milk_orders.php?msg=fail'</script>";
+      }
+
   }
 ?>
     <div class="site-content">
@@ -66,7 +74,7 @@ if (!isset($_POST['submit']))  {
                   </div>
                   <div class="form-group">
                     <label for="form-control-2" class="control-label">Cancel Date</label>
-                    <input type="text" class="form-control" id="cancel_date" name="cancel_date" placeholder="Cancel Date" data-error="Please enter Order Date." required>
+                    <input type="text" class="form-control" id="cancel_date1" name="cancel_date" placeholder="Cancel Date" data-error="Please enter Cancel Date." required class="cancel_date1">
                     <div class="help-block with-errors"></div>
                   </div>
                   <button type="submit" name="submit" value="Submit"  class="btn btn-primary btn-block">Submit</button>
@@ -97,5 +105,14 @@ if (!isset($_POST['submit']))  {
                   }
               });
           }
+      });
+      
+    </script>
+
+    <link href="https://cdn.rawgit.com/dubrox/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.css" rel="stylesheet"/>
+    <script src="https://cdn.rawgit.com/dubrox/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.js"></script>
+    <script type="text/javascript">
+      $( function() {
+          $('#cancel_date1').multiDatesPicker({ dateFormat: 'yy-mm-dd', minDate: 0 });
       });
     </script>
